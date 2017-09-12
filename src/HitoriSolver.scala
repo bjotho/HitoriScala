c55ae2cbe9962a24b533fdec5cb60445227b8faa
Bjorn Inge Thoresen <bithor15@uia.no>
2017-09-12 10:11:09 +0200

    removed Square.scala file, added parsing from input file to Square class in HitoriScala.scala

--- src/HitoriSolver.scala
+++ src/HitoriSolver.scala
@@ -5,48 +5,64 @@ object HitoriSolver
   def main(args: Array[String]): Unit =
   {
 
-    val inputPath = args(0)
-    val outputPath = args(1)
-    println(inputPath)
-    println(outputPath)
+    val inputPath = args(0);
+    val outputPath = args(1);
+    println(inputPath);
+    println(outputPath);
 
-    val puzzleFile = new File(inputPath)
+    val puzzleFile = new File(inputPath);
 
-    solvePuzzle(puzzleFile)
+    solvePuzzle(puzzleFile);
 
     def solvePuzzle(f:File):Unit = {
-      val lines = scala.io.Source.fromFile(f).mkString.split("\n")
-      //println(lines)
+      val lines = scala.io.Source.fromFile(f).mkString.split("\n");
+      //println(lines);
       //lines.foreach(print);
       
       //Solve Puzzle here:
       
       val p = new Puzzle(lines);
       p.fillPuzzle(lines);
+      println(p.getSquareList().foreach(_.v));
+      println(p.getSquareList()(3).v);
 
       // Solve puzzle and output to file, like so:
-      var outputFile = new PrintWriter( new File(outputPath) , "UTF-8")
+      var outputFile = new PrintWriter( new File(outputPath) , "UTF-8");
 
-      outputFile.println("B W W W W")
-      outputFile.println("W B W B W")
-      outputFile.println("W W W W B")
-      outputFile.println("W W B W W")
-      outputFile.println("B W W W B")
+      outputFile.println("B W W W W");
+      outputFile.println("W B W B W");
+      outputFile.println("W W W W B");
+      outputFile.println("W W B W W");
+      outputFile.println("B W W W B");
 
-      outputFile.close()
+      outputFile.close();
     }
   }
   
   class Puzzle(lines:Array[String])
   {
+    var allSquares = List[Square]();
     def fillPuzzle(lines:Array[String]) =
     {
       val n = lines.mkString.split(" ");
+      //n.foreach(println);
+      var i = 0;
+      while(i < n.length)
+      {
+        val s = new Square(1, 1, n(i));
+        allSquares = allSquares :+ s;
+        i += 1;
+      }
+    }
+    
+    def getSquareList():List[Square] =
+    {
+      return allSquares;
     }
   }
   
   
-  class Square(xPos:Int, yPos:Int, value:Int, possibleColors:List[Char] = List[Char]('B', 'W'), solved:Boolean = false)
+  class Square(xPos:Int, yPos:Int, value:String, possibleColors:List[Char] = List[Char]('B', 'W'), solved:Boolean = false)
   {
     val x = xPos;
     val y = yPos;
--- src/Square.scala
+++
@@ -1,7 +0,0 @@
-
-class Square(xPos:Int, yPos:Int, value:Int, solved:Boolean = false)
-{
-  val x = xPos;
-  val y = yPos;
-  val v = value;
-}
